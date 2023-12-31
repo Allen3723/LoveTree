d2c54ecfafec1d5b0babde68f82149ac5bf27216
Allen3723 <134393906+Allen3723@users.noreply.github.com>
2023-12-31 23:24:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,12 @@
 var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
-        "亲爱的雯雯",
-        "花开一季，有落红为证",
-        "逝去的岁月，如何能托于烟尘",
-        "你的浅笑，是我永生眷恋",
-        "相思的真情，是否能明白一二",
-        "亲爱的宝贝，你是我的唯一",
-        "爱你永远。。。",
+        "My heart beats for you every day and I am inspired by you every minute",
+        "Everyday I'm waiting for you",
+        "Even the endless darkness is enlightened by you",
+        "This is me praying that this was the very first page, not where the story line ends.",
+        "Wish you have a better 2024, and……",
+        "I like you",
     ],
     // 相爱的时间 记住格式不能写错了, 非常重要
     // 格式一定要和下面这个时间格式一样
@@ -16,7 +15,7 @@ var config = {
     //      温馨警告:
     //          1.时分秒都必须是俩个数字  比如 00:00:00
     //          2.T不能丢了
-    date: "2017-07-20T20:15:00",
+    date: "2023-07-20T20:40:00",
     // 双方的名字
-    names: ["笨笨熊", "黑猪猪"]
+    names: ["宸宸", "齐齐宝贝"]
 }
